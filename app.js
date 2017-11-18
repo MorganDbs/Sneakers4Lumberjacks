35385e0dace4a3d234d1be89d4d60558aaf18da1
MorganDbs <mailmorgandubois@gmail.com>
2017-11-18 17:08:57 +0100

    correction models, ajout fixtures

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ const multer = require('multer');
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
+require('./fixtures');
+
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
--- models/Sneakers.js
+++ models/Sneakers.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
 const sneakersSchema = new mongoose.Schema({
-  size:{type:Number,min:4,max:12},
-
-
+    brand: String,
+    model: String,
+    price: Number,
+    size: Number,
+    available: Boolean,
+    place: {
+        name: String,
+        lattitude: String,
+        longitude: String
+    }
 }, { timestamps: true });
 const Sneakers = mongoose.model('Sneakers', sneakersSchema);
 module.exports = Sneakers;
