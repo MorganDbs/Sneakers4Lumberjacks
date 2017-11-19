175021d9cbe0119f614541755f23eb5c4fb565fb
Quentin.prm <quentin.prm@gmail.com>
2017-11-19 23:11:47 +0100

    Auto stash before rebase of "master"
    template for sneakers page and maps with navbar link

--- controllers/sneakers.js
+++ controllers/sneakers.js
@@ -2,13 +2,15 @@ const Sneakers = require('../models/Sneakers.js');
 
 exports.getAllSneakerss = (req, res) => {
     Sneakers.find((err, docs) => {
-        res.render('sneakers', { sneakers: docs });
+        res.render('sneakers', {  sneakers: docs,title:'Sneakers'});
     });
 };
 
 exports.getSneakers = (req, res) => {
     var name = req.params.name;
     Sneakers.find({ model: name }, function (err, docs) {
-        res.render('sneakersDescription', { sneakersdes: docs });
+        res.render('sneakersDescription', {
+          sneakersdes: docs
+        });
     });
 };
